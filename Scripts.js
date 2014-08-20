94c3bef4fdac551fead9764313e0b6e40bc0d787
Andrew Hyte <hytead@mtc.byu.edu>
2014-08-20 11:03:45 -0600

    Google Login
    Added login capabilities with google OAuth2.
    
    Firebase-Simple-Login

--- Scripts.js
+++ Scripts.js
@@ -2,12 +2,41 @@ angular.module("myapp", [])
     .controller("HelloController", function($scope) {
         var fb = new Firebase("https://lovenotes.firebaseio.com");
 
+        var authClient = new FirebaseSimpleLogin(fb, function(error, user) {
+            if (error) {
+                // an error occurred while attempting login
+                console.log(error);
+            } else if (user) {
+                // user authenticated with Firebase
+                console.log("User ID: " + user.uid + ", Provider: " + user.provider);
+            } else {
+                // user is logged out
+            }
+        });
+
         fb.on('value', function (snapshot) {
             console.log(snapshot.val());
         }, function (errorObject) {
             console.log('The read failed: ' + errorObject.code);
         });
-        
+
+        var authRef = new Firebase("https://lovenotes.firebaseio.com/.info/authenticated");
+        authRef.on("value", function(snap) {
+            if (snap.val() === true) {
+                alert("authenticated");
+            } else {
+                alert("not authenticated");
+            }
+        });
+
+        $scope.auth = function(){
+            console.log(authClient.login('google', {
+                rememberMe: true,
+                scope: 'https://www.googleapis.com/auth/plus.login'
+            }));
+
+        };
+
         $scope.helloTo = {};
         $scope.helloTo.title = "Andrew, AngularJS";
     } );
